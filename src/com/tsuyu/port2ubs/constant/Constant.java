4ae202604cc9c85eb9b02fdd339794e5d4aa3489
tsuyu <mohamad.yusuf@hotmail.com>
2012-02-29 17:09:09 +0800

    Update src/com/tsuyu/port2ubs/constant/Constant.java

--- src/com/tsuyu/port2ubs/constant/Constant.java
+++ src/com/tsuyu/port2ubs/constant/Constant.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.kospek.port2ubs.constant;
+package com.tsuyu.port2ubs.constant;
 
 /**
  *
