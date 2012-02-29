efa2b05bc834cbf1f4950ded458228d45b7e4567
tsuyu <mohamad.yusuf@hotmail.com>
2012-02-29 17:12:20 +0800

    Update src/com/tsuyu/port2ubs/main/Port2UBSApp.java

--- src/com/tsuyu/port2ubs/main/Port2UBSApp.java
+++ src/com/tsuyu/port2ubs/main/Port2UBSApp.java
@@ -3,7 +3,7 @@
  * and open the template in the editor.
  */
 
-package com.kospek.port2ubs.main;
+package com.tsuyu.port2ubs.main;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
